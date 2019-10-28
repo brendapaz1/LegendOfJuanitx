0fb43b342ed8bb1fdc2a9d28930d8f4f7e5e3cff
brendapaz1 <brendapaz1771@gmail.com>
2019-10-27 23:30:29 -0700

    Primero
    Ya tiene mis backgrounds

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public CharStats[] playerStats;
+
+    public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
+
+    public string[] itemsHeld;
+    public int[] numberOfItems;
+    public Item[] referenceItems;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        SortItems();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameMenuOpen || dialogActive || fadingBetweenAreas)
+        {
+            PlayerController.instance.canMove = false;
+        }
+        else
+        {
+            PlayerController.instance.canMove = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            AddItem("Iron Armor");
+            AddItem("Blala");
+
+            RemoveItem("Health Potion");
+            RemoveItem("BSSA");
+        }
+    }
+
+    public Item GetItemDeatils(string itemToGrab)
+    {
+        for (int i = 0; i < referenceItems.Length; i++)
+        {
+            if (referenceItems[i].itemName == itemToGrab)
+            {
+                return referenceItems[i];
+            }
+        }
+
+
+
+
+        return null;
+    }
+
+    public void SortItems()
+    {
+        bool itemAfterSpace = true;
+
+        while (itemAfterSpace)
+        {
+
+            for (int i = 0; i < itemsHeld.Length - 1; i++)
+            {
+                itemAfterSpace = false; 
+                if (itemsHeld[i] == "")
+                {
+                    itemsHeld[i] = itemsHeld[i + 1];
+                    itemsHeld[i + 1] = "";
+
+                    numberOfItems[i] = numberOfItems[i + 1];
+                    numberOfItems[i + 1] = 0;
+                    
+                    if(itemsHeld[i]!="")
+                    {
+                        itemAfterSpace = true;
+                    }
+                }
+            }
+        }
+    }
+    public void AddItem(string itemToAdd)
+    {
+        int newItemPosition = 0;
+        bool foundSpace = false;
+
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            if (itemsHeld[i] == "" || itemsHeld[i] == itemToAdd)
+            {
+                newItemPosition = i;
+                i = itemsHeld.Length;
+                foundSpace = true;
+            }
+        }
+
+        if(foundSpace)
+        {
+            bool itemExists = false;
+            for( int i = 0; i < referenceItems.Length;i++)
+            {
+                if(referenceItems[i].itemName == itemToAdd)
+                {
+                    itemExists = true;
+
+                    i = referenceItems.Length;
+                }
+
+            }
+            if(itemExists)
+            {
+                itemsHeld[newItemPosition] = itemToAdd;
+                numberOfItems[newItemPosition]++;
+            }else
+            {
+                Debug.LogError(itemToAdd + "Does Not Exist!!");
+            }
+        }
+
+        GameMenu.instance.ShowItems();
+    }
+
+    public void RemoveItem(string itemToRemove)
+    {
+        bool foundItem = false;
+        int itemPosition = 0;
+
+        for(int i =0; i < itemsHeld.Length;i++)
+        {
+            if(itemsHeld[i] == itemToRemove)
+            {
+                foundItem = true;
+                itemPosition = i;
+
+                i = itemsHeld.Length;
+            }
+        }
+        if(foundItem)
+        {
+            numberOfItems[itemPosition]--;
+
+            if(numberOfItems[itemPosition] <=0)
+            {
+                itemsHeld[itemPosition] = "";
+            }
+
+            GameMenu.instance.ShowItems();
+        }else
+        {
+            Debug.LogError("Couldn't find " + itemToRemove);
+        }
+    }
+}
